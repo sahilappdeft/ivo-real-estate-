168a5e3cd23ec7392190f36f23ae5ebbdfd284ba
unknown <sahilrawat049@gmail.com>
2024-06-20 15:00:11 +0530

    cors

--- ivo/settings/base.py
+++ ivo/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-l&2=xd$u9w9fr*9z$h1wecvagy9a%sn9yot(7(9zo*c-)u0u16
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
