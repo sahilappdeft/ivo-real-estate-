ca51a0c3615fcdee25cccc2d1e9322fab20f095e
unknown <sahilrawat049@gmail.com>
2024-06-26 17:53:59 +0530

    company spelling

--- auth_user/serializers.py
+++ auth_user/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    comapny = serializers.IntegerField(source='employee.company.id')
+    company = serializers.IntegerField(source='employee.company.id')
     
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'comapny']
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'company']
         
     
